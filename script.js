cbd20f9bb737fa26660ce52d4ba06f48700c9115
Shyam Venkatesh <shyamvenkat007@gmail.com>
2023-02-13 14:36:02 +0530

    Added table-wise display for stats

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
+/*
+***************************
+********Todos******
+1. Profile picture
+2. Better UI
+***************************
+*/
+
+/*
+***************************
+********The headers are added with authorisation and JSON method******
+***************************
+*/
 let myHeaders = new Headers();
 myHeaders.append("ApeKey", "monkeytype-trial");
 myHeaders.append('Accept', 'application/json');
 
+/*
+***************************
+********This object is passed as options to the fetch call******
+***************************
+*/
 let requestOptions = {
   'method': 'GET',
   'headers': myHeaders,
   'redirect': 'follow'
 };
 
+/*
+***************************
+********This function simply displays details directly to a <p> tag******
+***************************
+*/
 function handleData(result) {
   let data = result;
   let userElem = document.getElementById("username-placeholder");
@@ -44,10 +67,52 @@ function handleData(result) {
   }
 }
 
+function handleTableData(result) {
+  let data = result;
+
+  let userElem = document.getElementById("username-placeholder");
+  console.log(data);
+  userElem.innerText = data.data.name;
+
+  let attributesArray = ['acc', 'wpm', 'difficulty', 'language'];
+  let subOptionArray1 = ['30', '60', '120'];
+
+  /*
+  ***************************
+  ********This fills the first table with bests of TIME mode******
+  ***************************
+  */
+  for(let item of attributesArray) {
+    for(let i = 0; i < 3; i++) {
+      let selector = "table1-row" + (i+1);
+      let rowElem = document.getElementById(selector);
+      console.log(document.getElementById(selector));
+      rowElem.innerHTML += `<td>${data.data.personalBests.time[subOptionArray1[i]]['0'][item]}</td>`;
+    }
+  }
+
+  /*
+  ***************************
+  ********This fills the first table with bests of WORDS mode******
+  ***************************
+  */
+  let subOptionArray2 = ['50', '100'];
+  for(let item of attributesArray) {
+    for(let i = 0; i < 2; i++) {
+      let selector = "table2-row" + (i+1);
+      let rowElem = document.getElementById(selector);
+      console.log(document.getElementById(selector));
+      rowElem.innerHTML += `<td>${data.data.personalBests.words[subOptionArray2[i]]['0'][item]}</td>`;
+    }
+  }
+
+}
+
 fetch("https://api.monkeytype.com/users/sheeeshtyper/profile", requestOptions)
   .then(response => response.json())
   .then(result => {
     console.log(result);
-    handleData(result);
+    // handleData(result);
+    handleTableData(result);
   })
   .catch(error => console.log('error', error));
